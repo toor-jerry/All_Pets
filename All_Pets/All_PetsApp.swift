2291bdb88c92415141b002e2af6d85648eca012b
Gerardo Bautista <gerarchicharo37@gmail.com>
2023-08-08 18:51:58 -0600

    Code base

---
+++ All_Pets/All_PetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  All_PetsApp.swift
+//  All_Pets
+//
+//  Created by 1058889 on 08/08/23.
+//
+
+import SwiftUI
+
+@main
+struct All_PetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
