5260600e3a41144cddf3b3f0c479103a8ab4cc5d
Gerardo Bautista <gerarchicharo37@gmail.com>
2023-08-11 16:54:31 -0600

    Add show or hide

--- All_Pets/Components/Login/LoginView/LoginFormView.swift
+++ All_Pets/Components/Login/LoginView/LoginFormView.swift
@@ -8,47 +8,49 @@
 import SwiftUI
 
 struct LoginFormView: View {
-
+    
     @StateObject var viewModel: LoginViewModel
-
+    
     @State private var userName: String = ""
     @State private var password: String = ""
     @State private var showPassword = false
-
+    
     var body: some View {
         VStack {
             Image(Constants.logo)
                 .resizable()
                 .scaledToFit()
                 .frame(height: 300)
-
+            
             TextField(String.MsgEmailLogin, text: $userName)
                 .padding()
                 .modifier(inputStylePrincipal())
                 .keyboardType(.emailAddress)
                 .disableAutocorrection(true)
                 .padding(.horizontal, 40)
-
-            HStack {
-                if showPassword {
-                    TextField(String.MsgPasswordLogin, text: $password)
-                        .padding()
-                        .modifier(inputStylePrincipal())
-                        .disableAutocorrection(true)
-                        .textContentType(.password)
-                        .keyboardType(.asciiCapable)
-                } else {
-                    SecureField(String.MsgPasswordLogin, text: $password)
-                        .padding()
-                        .modifier(inputStylePrincipal())
+            
+            VStack {
+                VStack {
+                    if showPassword {
+                        TextField(String.MsgPasswordLogin, text: $password)
+                            .disableAutocorrection(true)
+                            .textContentType(.password)
+                            .keyboardType(.asciiCapable)
+                    } else {
+                        SecureField(String.MsgPasswordLogin, text: $password)
+                    }
                 }
-
-                ShorOrHideButton(showPassword: $showPassword)
-                    .padding(5)
+                .padding()
+                .padding(.trailing, 50)
+                .modifier(inputStylePrincipal())
             }
+            .overlay(alignment: .trailing, content: {
+                ShorOrHideButton(showPassword: $showPassword)
+                    .padding(.trailing, 20)
+            })
             .padding(.top, 15)
             .padding(.horizontal, 40)
-
+            
             Button(action: {
                 viewModel.login(info: AuthLoginInfo(password: password, user: userName))
             }, label: {
@@ -57,25 +59,25 @@ struct LoginFormView: View {
             })
             .modifier(buttonPrincipal())
             .padding(.top, 20)
-
+            
             Button(action: {
-
+                
             }, label: {
                 Text(String.MsgForgotPassword)
                     .modifier(textStyleSubtitle())
-
+                
             })
             .padding(.top, 15)
-
+            
             Button(action: {
-
+                
             }, label: {
                 Text(String.MsgSignUpTitle)
                     .modifier(textStyleSubtitle())
-
+                
                 Text(String.MsgSignUp)
                     .modifier(textStyleTitle2())
-
+                
             })
             .padding(.top, 60)
             .alert(isPresented: $viewModel.showAlert) {
