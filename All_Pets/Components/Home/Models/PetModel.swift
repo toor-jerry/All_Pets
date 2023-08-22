6054aef422d08ee9fab4399dab11c2cf9135102c
Gerardo Bautista <gerarchicharo37@gmail.com>
2023-08-21 18:39:07 -0600

    Add vaccination card

---
+++ All_Pets/Components/Home/Models/PetModel.swift
@@ -0,0 +1,25 @@
+//  PetModel.swift
+//  All_Pets
+//
+//  Created by Gerardo Bautista Castañeda on 21/08/23.
+//  Copyright © 2023 ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import Firebase
+
+struct Pet: Codable {
+
+    var birthDate: Timestamp
+    var pet: String
+    var petType: String?
+    var name: String?
+    var photoURL: String?
+
+    enum CodingKeys: String, CodingKey {
+        case name = "Nombre"
+        case birthDate = "Fecha_Nacimiento"
+        case photoURL = "ImgUrl"
+        case petType = "Raza"
+        case pet = "Mascota"
+    }
+}
--- All_Pets/Components/Home/Models/UserModel.swift
+++ All_Pets/Components/Home/Models/UserModel.swift
@@ -5,8 +5,6 @@
 //  Copyright © 2023 ___ORGANIZATIONNAME___. All rights reserved.
 //
 
-import Firebase
-
 struct User: Codable {
 
     let name: String
@@ -17,22 +15,5 @@ struct User: Codable {
 
     init(name: String = "") {
         self.name = name
-    }
-}
-
-struct Pet: Codable {
-
-    var birthDate: Timestamp
-    var pet: String
-    var petType: String?
-    var name: String?
-    var photoURL: String?
-
-    enum CodingKeys: String, CodingKey {
-        case name = "Nombre"
-        case birthDate = "Fecha_Nacimiento"
-        case photoURL = "ImgUrl"
-        case petType = "Raza"
-        case pet = "Mascota"
     }
 }
