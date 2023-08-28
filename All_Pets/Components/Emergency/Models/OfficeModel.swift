0f35d9cf3f51341f77182f24765f8c676ca68ff9
Gerardo Bautista <gerarchicharo37@gmail.com>
2023-08-28 17:30:45 -0600

    Emergency use case

---
+++ All_Pets/Components/Emergency/Models/OfficeModel.swift
@@ -0,0 +1,32 @@
+//  Emergency.swift
+//  All_Pets
+//
+//  Created by Gerardo Bautista Castañeda on 28/08/23.
+//  Copyright © 2023 ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import Foundation
+
+struct OfficeModel: Codable {
+    var status: String
+    var hourEnd: String
+    var hourStart: String
+    var idOffice: String
+    var latitude: String
+    var length: String
+    var name: String
+    var medicalSpecialities: String?
+    var specializedSector: String?
+
+    enum CodingKeys: String, CodingKey {
+        case status = "status"
+        case hourEnd = "HFin"
+        case hourStart = "HInicio"
+        case idOffice = "id"
+        case latitude = "Latitud"
+        case length = "Longitud"
+        case name = "Nombre"
+        case medicalSpecialities = "medical_specialities"
+        case specializedSector = "specialized_sector"
+    }
+}
---
+++ All_Pets/Helpers/NetworkingProvider/UseCases/EmergencyUseCase.swift
@@ -0,0 +1,54 @@
+//  EmergencyUseCase.swift
+//  All_Pets
+//
+//  Created by Gerardo Bautista Castañeda on 28/08/23.
+//  Copyright © 2023 ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import Firebase
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+
+protocol OfficeProtocol: AnyObject {
+    func getOffices(success: @escaping (_ offices: [OfficeModel]) -> Void,
+                    failure: @escaping (Error) -> Void,
+                    completion: @escaping () -> Void)
+}
+
+protocol EmergencyUseCaseProtocol: OfficeProtocol { }
+
+final class EmergencyUseCase: EmergencyUseCaseProtocol { }
+
+extension EmergencyUseCase: OfficeProtocol {
+
+    func getOffices(success: @escaping ([OfficeModel]) -> Void,
+                    failure: @escaping (Error) -> Void,
+                    completion: @escaping () -> Void) {
+
+        let db = Firestore.firestore()
+
+        let officeCollection = db.collection(Endpoint.usersCollection.urlString)
+
+        officeCollection.getDocuments { querySnapshot, error in
+
+            if let error = error {
+                failure(error)
+            } else {
+                var offices: [OfficeModel] = []
+
+                for document in querySnapshot?.documents ?? [] {
+                    do {
+                        let office = try document.data(as: OfficeModel.self)
+                        offices.append(office)
+                    } catch {
+                        print("Errors: ", NetworkingClientErrors.decodingError)
+                    }
+                }
+
+                success(offices)
+            }
+
+            completion()
+        }
+    }
+}
