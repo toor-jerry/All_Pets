baa1b117a7b12af41fa813f90a49bacf56a10e24
Gerardo Bautista <gerarchicharo37@gmail.com>
2023-08-29 10:34:43 -0600

    add location manager

--- All_Pets/Components/Emergency/EmergencyView/EmergencyView.swift
+++ All_Pets/Components/Emergency/EmergencyView/EmergencyView.swift
@@ -30,4 +30,3 @@ struct EmergencyViewPreviews: PreviewProvider {
         EmergencyView()
     }
 }
-
--- All_Pets/Components/Emergency/EmergencyViewModel/EmergencyViewModel.swift
+++ All_Pets/Components/Emergency/EmergencyViewModel/EmergencyViewModel.swift
@@ -6,15 +6,32 @@
 //
 
 import SwiftUI
+import CoreLocation
+import MapKit
 
-final class EmergencyViewModel: ObservableObject {
+final class EmergencyViewModel: NSObject, ObservableObject {
+
+    private struct Span {
+        static let delta = 0.1
+    }
 
     let useCase: EmergencyUseCaseProtocol
 
     @Published var isLoading: Bool = false
+    @Published var office: OfficeModel = OfficeModel()
+    @Published var officeLocation: MKCoordinateRegion = .init()
+    @State var userLocation: MKCoordinateRegion = .init()
+    private let locationManager: CLLocationManager = .init()
 
     init(useCase: EmergencyUseCaseProtocol) {
         self.useCase = useCase
+
+        super.init()
+        
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        locationManager.delegate = self
     }
 
     func getOffices() {
@@ -28,5 +45,14 @@ final class EmergencyViewModel: ObservableObject {
                 self.isLoading = false
             }
         })
+    }
+}
+
+extension EmergencyViewModel: CLLocationManagerDelegate {
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        print("Data2 location: ", location)
+        userLocation = .init(center: location.coordinate, span: .init(latitudeDelta: Span.delta, longitudeDelta: Span.delta))
     }
 }
