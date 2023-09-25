4e1139b50c09b3c8b3d202363d97522d3ea33c64
Gerardo Bautista <gerarchicharo37@gmail.com>
2023-09-25 17:31:36 -0600

    Create session info

---
+++ All_Pets/Helpers/SessionInfo/SessionInfoCustom.swift
@@ -0,0 +1,16 @@
+//  SessionInfoCustom.swift
+//  All_Pets
+//
+//  Created by Gerardo Bautista Castañeda on 25/09/23.
+//  Copyright © 2023 ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import UIKit
+
+class SessionInfoCustom {
+    
+    func templateFunction() {
+        
+    }
+    
+}
