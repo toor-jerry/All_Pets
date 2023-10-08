2ce416dbeedab93e38049be9529e705afcd9419c
Gerardo Bautista <gerarchicharo37@gmail.com>
2023-10-07 20:54:53 -0600

    Send request cite

---
+++ All_Pets/Helpers/Extensions/Date+Extension.swift
@@ -0,0 +1,16 @@
+//  Date+Extension.swift
+//  All_Pets
+//
+//  Created by Gerardo Bautista Castañeda on 07/10/23.
+//  Copyright © 2023 ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import UIKit
+
+class Date_Extension {
+    
+    func templateFunction() {
+        
+    }
+    
+}
---
+++ All_Pets/Helpers/NetworkingProvider/UseCases/CreateAppoimentUseCase.swift
@@ -0,0 +1,16 @@
+//  CreateAppoimentUseCase.swift
+//  All_Pets
+//
+//  Created by Gerardo Bautista Castañeda on 07/10/23.
+//  Copyright © 2023 ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import UIKit
+
+class CreateAppoimentUseCase {
+    
+    func templateFunction() {
+        
+    }
+    
+}
