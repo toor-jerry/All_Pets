574bb08eedac6d5b6ec75551c62440b2bab2a8c5
Gerardo Bautista <gerarchicharo37@gmail.com>
2023-10-09 18:20:47 -0600

    Add explore sections

---
+++ All_Pets/Helpers/NetworkingProvider/UseCases/ExploreUseCase.swift
@@ -0,0 +1,16 @@
+//  ExploreUseCase.swift
+//  All_Pets
+//
+//  Created by Gerardo Bautista Castañeda on 09/10/23.
+//  Copyright © 2023 ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import UIKit
+
+class ExploreUseCase {
+    
+    func templateFunction() {
+        
+    }
+    
+}
